b370dbd0af63ae39ebc952201219b4339f45a465
elementair <element.earias20@gmail.com>
2021-01-12 19:06:29 -0600

    v1.1 cap5

--- mi_primera_app/lib/main.dart
+++ mi_primera_app/lib/main.dart
@@ -26,37 +26,65 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Filas, Columnas y botones"),
-      ),
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Container(
-          
-            child:Text("Hola 01", textAlign: TextAlign.center,) ,
-          ),
-      
-          Text("Hola 02"),
-          Text("Hola 03"),
-          Text("Hola 04"),
-          Text("Hola 05")
-
-        ],
-      
-      ) 
+     
+      body:cuerpo(),
     );
   }
 }
 
+Widget cuerpo(){
+  return Container(
+
+    decoration: BoxDecoration(
+      image: DecorationImage(image: NetworkImage("https://i.pinimg.com/originals/1a/bf/40/1abf40c40296e293eb8d4a100da4a9fa.jpg"),
+      fit: BoxFit.cover,
+      ),
+    ),
+    child: Center(
+      child: nombre(),
+      
+      ),
+  );
+}
+
+Widget nombre(){
+  return Text("Sing in", style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold));
+}
+
+Widget campoUsuario(){
+
+  return Container(
+    child: TextField(
+     decoration: InputDecoration(
+       hintText: "Usuario",
+       fillColor: Colors.white,
+       filled: true,
+
+     ),
+
+    ),
+  );
+
+}
+
+
+
+
+
+
+Widget imagen(){
+  return Container(
+     padding: EdgeInsets.all(10.0),
+       child: Image.network("https://www.generadormemes.com/media/created/xyscrsqh6b74sdsic4bllb30rac5td7l8sj5p4aord12vqie61csh95id0gb59gu.jpg.pagespeed.ic.imagenes-memes-fotos-frases-graciosas-chistosas-divertidas-risa-chida-español-whatsapp-facebook.jpg"),
+
+  );
+}
+
 
 /*
-IMAGENES
-
- body:  ListView(
+  IMAGENES
+  body:  ListView(
    children: [
-
     Container(
        padding: EdgeInsets.all(10.0),
        child: Image.network("https://www.generadormemes.com/media/created/xyscrsqh6b74sdsic4bllb30rac5td7l8sj5p4aord12vqie61csh95id0gb59gu.jpg.pagespeed.ic.imagenes-memes-fotos-frases-graciosas-chistosas-divertidas-risa-chida-español-whatsapp-facebook.jpg"),
@@ -64,3 +92,45 @@ IMAGENES
    ],
  )
  */
+/*
+  FILAS Y COLUMNAS
+   body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child:Text("Hola 01", textAlign: TextAlign.center,) ,
+          ),
+          Text("Hola 02"),
+        ],
+      ) 
+*/
+
+/*
+  Boton
+  body:Center(
+        child: RaisedButton(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(Icons.access_time),
+              SizedBox(
+                height: 7,
+              ),
+              Text("Now"),
+              
+            ],
+          ),
+
+          onPressed: (){
+
+            var t =DateTime.now();
+            print(t);
+
+          },
+        ),
+      
+      ) 
+*/
